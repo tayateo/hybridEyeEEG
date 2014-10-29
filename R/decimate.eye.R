bde70e4d1eaca248c82a5253608c96e72a2905c9
Nastya <mayenok@gmail.com>
2014-10-29 16:19:49 +0300

    working on decimating a vector containing NAs 0_0

--- R/decimate.eye.R
+++ R/decimate.eye.R
@@ -2,9 +2,9 @@ decimate.eye <- function(filename, q, old = F, fixation.duration = NULL)
 {
   
   #q  - integer factor to downsample by.
-  lines <- load.edf(sprintf("%s.edf",filename))
-  ans <- extract.samples(lines, old, fixation.duration)
-  points <- ans$points
+
+  ans <- load.one.eye(filename)
+  points <- ans$samples
   decimated.points <- data.frame(x = downsample.vector(points$x, q),
                                  y = downsample.vector(points$y, q),
                                  pupil = downsample.vector(points$pupil, q))
--- R/downsample.vector.R
+++ R/downsample.vector.R
@@ -3,7 +3,10 @@ downsample.vector <- function(vect, q)
   v <- data.frame(t = 1:length(vect), vect = vect)
   
   v.withoutNA <- v[complete.cases(v),]
+  v.withNA <- v[!complete.cases(v),]
   
   dec.vect <- decimate(v.withoutNA$vect, q)
+  
+
   
 }
