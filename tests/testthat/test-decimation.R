7a2ad1de72b8c1f23226f2c5c34ce803decfaf2e
Nastya <mayenok@gmail.com>
2014-10-30 13:28:19 +0300

    working on NAs in the beginning of the vector

--- tests/testthat/test-decimation.R
+++ tests/testthat/test-decimation.R
@@ -1,6 +1,50 @@
-test_that("decimation", {
+test_that("decimation_test1", {
+  
   full <- c(0, 0, 0, 0, NA, NA, 0, 0)
   short <- downsample.vector(full, 2)
   
   expect_equal(short, c(0,0,NA,0))
+})
+
+test_that("decimation_test2", {
+  
+  full <- c(0, 0, 0, NA, NA, NA, 0, 0)
+  short <- downsample.vector(full, 2)
+  
+  expect_equal(short, c(0,0,NA,0))
+})
+
+
+test_that("decimation_test3", {
+  
+  full <- c(0, 0, 0, NA, NA, NA, 0, 0, NA, 0, NA)
+  short <- downsample.vector(full, 2)
+  
+  expect_equal(short, c(0,0,NA,0, NA, NA))
+})
+
+
+test_that("decimation_test4", {
+  
+  full <- c(0, 0, 0, NA, NA, NA, 0, 0, NA, 0, NA, NA, NA)
+  short <- downsample.vector(full, 2)
+
+  expect_equal(short, c(0,0,NA,0, NA, NA, NA))
+})
+
+test_that("decimation_test5", {
+  
+  full <- c(0, 0, 0, NA, NA, NA, 0, 0, NA, 0, NA, NA, NA, NA, NA)
+  short <- downsample.vector(full, 2)
+  
+  expect_equal(short, c(0,0,NA,0, NA, NA, NA, NA))
+})
+
+
+test_that("decimation_test6", {
+  
+  full <- c(NA, NA, NA, 0, 0, 0, NA, NA, NA, 0, 0, NA, 0, NA, NA, NA)
+  short <- downsample.vector(full, 2)
+  
+  expect_equal(short, c(NA, NA,  0, NA, NA,  0,  0, NA))
 })
